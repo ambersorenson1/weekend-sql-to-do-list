27ace124e53016ad0468bc2b6bb4b05b717be046
ambersorenson1 <ambersorenson1@gmail.com>
2021-11-19 14:23:19 -0600

    set up click listener and updated some functionality...also added GET route

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,5 +2,44 @@ $(document).ready(onReady);
 
 function onReady () {
   console.log('JQ is working');
-  
+  setupClickListeners();
 }
+
+function setupClickListeners() {
+  $('#addButton').on( 'click', function() {
+    console.log('in addButton on click');
+    let taskToBeCompleted = {
+      task: $('#taskIn').val(),
+      instructions: $('#instructionsIn').val(),
+    }
+    clearInputs();
+  });
+  };
+
+  function clearInputs()  {
+    $('#taskIn').val(''),
+    $('#instructionsIn').val(''),
+    console.log('Inputs cleared');
+  };
+  
+  function getTasks() {
+    $.ajax({
+      type: 'GET',
+      url: '/tasks'
+    }).then(function(response) {
+      // console.log(response);
+      renderTasks(response);
+    }).catch(function(error){
+      console.log('error in GET', error);
+    });
+  };
+
+  function renderTasks(weekendToDoList) {
+    console.log('in renderTasks');
+    $('#viewToDoItems').empty();
+  
+    for (let i=0; i<weekendToDoList.length; i++) {
+      let weekendToDoList = weekendToDoList[i];
+      //readyOrNot(koala);
+    }
+  } // end renderTasks
--- server/routes/weekendToDoApp.router.js
+++ server/routes/weekendToDoApp.router.js
@@ -5,3 +5,19 @@ const weekendToDoAppRouter = express.Router();
 
 const pool = require('../modules/pool.js');
 module.exports = weekendToDoAppRouter;
+
+//GET
+// GET
+weekendToDoAppRouter.get('/', (req, res) => {
+  console.log('GET /tasks');
+  const text = 'SELECT * FROM weekend-to-do-app;';
+  pool.query(text)
+    .then((dbResult) => {
+     //  console.log(`${dbResult.rows.length} rows to send.`)
+      res.send(dbResult.rows);
+    })
+    .catch((dbErr) => {
+      console.error(dbErr);
+      res.sendStatus(500);
+    });
+});
